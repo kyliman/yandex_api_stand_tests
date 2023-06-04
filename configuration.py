baeab5916fecdc9dd7af799c4ae704397eb8ea4b
Анастасия Куликова <kyliman@MacBook-Air-Anastasia.local>
2023-06-04 18:03:51 +0400

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://c81f9a4a-c72b-4fbb-8cb5-dc6837f94dce.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,21 @@
+import configuration
+import requests
+import data
+
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, json=products_ids,
+                         headers=data.headers)
+
+
+def post_new_user(user_body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH, json=user_body,
+                         headers=data.headers)
+
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+# response = post_products_kits(data.product_ids);
+# print(response.status_code)
+# print(response.json())
